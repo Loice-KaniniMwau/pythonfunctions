583500bd75f881adff1ddecee9ede2a796ffa199
Loice-KaniniMwau <mwauloice@gmail.com>
2023-03-29 19:36:48 +0300

    functions assignment

---
+++ functions/hello.py
@@ -0,0 +1,6 @@
+def hello(name):
+    print(f"hello{name}")
+def welcome(student,school):
+    print(f"welcome to {school},{student}")
+def identity(name,age,height):
+    print(f"my name is {name},and i am {age} years,and my height is{height}")
---
+++ functions/math.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    answer=a+b
+    return answer
+def multiply(a,b):
+    product=a*b
+    return product
+def subtract(a,b):
+    substraction=a-b
+    return substraction
+def divide(a,b):
+    division=a/b
+    return division
+def remainder(a,b):
+    theremainder=a%b
+    return theremainder
